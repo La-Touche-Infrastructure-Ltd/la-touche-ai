fbaf45915909e26e986f3fa46bb38e3675098e18
LA TOUCHE INFRASTRUCTURE <253608020+LaToucheai@users.noreply.github.com>
2026-05-10 23:46:39 +0100

    feat: build notification inbox UI and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import CookieBanner from "./components/CookieBanner";
 import About from "./pages/About";
 import Trademark from "./pages/Trademark";
+import Notifications from "./pages/Notifications";
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import Privacy from "./pages/Privacy";
 import Cookies from "./pages/Cookies";
-
 import Terms from "./pages/Terms";
 import NotFound from "./pages/NotFound";
 
@@ -20,23 +23,27 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+
       <BrowserRouter>
-    <Routes>
-  <Route path="/" element={<Index />} />
-  <Route path="/about" element={<About />} />
-  <Route path="/privacy" element={<Privacy />} />
-  
-  <Route path="/cookies" element={<Cookies />} />
-  <Route path="/terms" element={<Terms />} />
-  <Route path="/trademark" element={<Trademark />} />
-  <Route path="*" element={<NotFound />} />
-</Routes>
-     </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/cookies" element={<Cookies />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/trademark" element={<Trademark />} />
 
-{/* <CookieBanner /> */}
+          <Route
+            path="/notifications"
+            element={<Notifications />}
+          />
 
-</TooltipProvider>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
 
+      {/* <CookieBanner /> */}
+    </TooltipProvider>
   </QueryClientProvider>
 );
 
